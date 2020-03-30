8101ac0675e385c5209fc8b350201b0964b6f64c
arpit191001 <60779810+arpit191001@users.noreply.github.com>
2020-03-30 16:57:25 +0530

    Update combo.java

--- combo.java
+++ combo.java
@@ -5,7 +5,7 @@ public class combo {
 JFrame f;    
 combo(){    
     f=new JFrame("ComboBox Example");    
-    String country[]={"India","Aus","U.S.A","England","Newzealand"};        
+    String country[]={"India","Aus","U.S.A","England","Newzealand","Finland","Denmark",};        
     JComboBox cb=new JComboBox(country);    
     cb.setBounds(50, 50,90,20);    
     f.add(cb);        
