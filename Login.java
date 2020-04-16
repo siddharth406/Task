f03cc533dac15ecea19bf79e35d7742a4d96c4e9
siddharth406 <60779906+siddharth406@users.noreply.github.com>
2020-04-16 22:56:44 +0300

    LOGIN

---
+++ Login.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @param request 
+	 * @throws  
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletRequest request, ServletResponse response) throws ServletException, IOException  {
+		// TODO Auto-generated method stub
+	
+
+	response.setContentType("text/html");
+	PrintWriter pw = response.getWriter();
+	
+	request.getRequestDispatcher("Logout").include(request,response);
+	
+	pw.print("<html>"
+			+ "<head><title>Login</title>"
+            + "<style>"
+			+"body(font-family:arial;)"
+			+"</style>"
+			+"</head>"
+			+"<center>"
+            +"<h1>Login</h1>"
+			+"<form action='Check_User' method='post'>"
+            +"<input type='text' name='unmae'placeholder='Username' required>"
+			+"<input type='password' name='pass'placeholder='Password' required>"
+            +"<input type='submit' value='Go'>"
+			+"</form>"
+			+ "</center"
+			+"</body></html>"
+			
+			);
+	
+	
+	}
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
